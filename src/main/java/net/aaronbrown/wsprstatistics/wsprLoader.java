6f25c467bd782305276adbdf8a97c4299e7e380a
aaron <aaronbrown1988@gmail.com>
2017-01-07 15:52:31 +1100

    Move to using application properties for configuration

--- src/main/java/net/aaronbrown/wsprstatistics/wsprLoader.java
+++ src/main/java/net/aaronbrown/wsprstatistics/wsprLoader.java
@@ -3,13 +3,7 @@ package net.aaronbrown.wsprstatistics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-
-import javax.sql.DataSource;
 
 
 @SpringBootApplication
@@ -21,14 +15,6 @@ public class wsprLoader {
         SpringApplication.run(wsprLoader.class, args);
     }
 
-    @Bean
-    public DataSource dataSource() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = builder
-                .setType(EmbeddedDatabaseType.HSQL) //.H2 or .DERBY
-                .build();
-        return db;
-    }
 //    @Bean
 //    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 //        return args -> {
