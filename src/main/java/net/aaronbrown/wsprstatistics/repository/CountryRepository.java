04ddb6d477adcbfea17d718a544f903915e5d234
aaron <aaronbrown1988@gmail.com>
2017-01-10 20:46:51 +1100

    Fix spot_time to datetime to allow for direct loading of data. Fix up missing imports from previous commit.

--- src/main/java/net/aaronbrown/wsprstatistics/repository/CountryRepository.java
+++ src/main/java/net/aaronbrown/wsprstatistics/repository/CountryRepository.java
@@ -1,14 +1,19 @@
 package net.aaronbrown.wsprstatistics.repository;
 
 import net.aaronbrown.wsprstatistics.entity.Country;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  * Callsign Repository holds data about which callsign map to which countries.
  *
  */
+@Repository
 public interface  CountryRepository extends CrudRepository<Country, Long> {
 
-    @Query("select dxcc from DXCC dxcc where startHash >= :prefix and endHash<=:prefix")
-    public Country getCountryForPrefix(@Param(":prefix") Integer prefix);
+    @Query("select country from Country country where country.startHash >= :prefix and country.endHash<=:prefix")
+    Country getCountryForPrefix(@Param("prefix") Integer prefix);
 
 }
