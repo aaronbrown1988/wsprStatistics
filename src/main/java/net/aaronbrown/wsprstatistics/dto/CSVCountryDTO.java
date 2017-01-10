2778528db630a0571c118d3b0b7c377802ab1a5b
Aaron <aaronbrown1988@gmail.com>
2017-01-10 19:59:28 +1100

    Random Train Musings

---
+++ src/main/java/net/aaronbrown/wsprstatistics/dto/CSVCountryDTO.java
@@ -0,0 +1,23 @@
+package net.aaronbrown.wsprstatistics.dto;
+
+import net.aaronbrown.wsprstatistics.entity.Country;
+
+/**
+ *
+ *
+ */
+public class CSVCountryDTO {
+
+    public static Country countryFromCSV(CSVRecord record) {
+        if (record == null) {
+            return null;
+        }
+        Integer i = 0;
+        Country country = new Country();
+        country.setCountryName(record.get(i));
+        country.setCountryCode(record.get(++i));
+        country.setStartPrefix(record.get(++i));
+        country.setEndPrefix(record.get(++i));
+        return country;
+    }
+}
---
+++ src/main/java/net/aaronbrown/wsprstatistics/services/CountryService.java
@@ -0,0 +1,64 @@
+package net.aaronbrown.wsprstatistics.services;
+
+import net.aaronbrown.wsprstatistics.dto.CSVCountryDTO;
+import net.aaronbrown.wsprstatistics.entity.Country;
+import net.aaronbrown.wsprstatistics.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/**
+ *
+ * This service is to help to look up the country a callsign belongs to
+ *
+ */
+@Service
+public class CountryService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryService.class);
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public String getCountryNameForCall(String callsign) {
+            Country country = countryRepository.getCountryForPrefix(getPrefixCode(callsign));
+            if (country == null) {
+                LOGGER.info("No country found for callsign:"+callsign);
+            }
+            return country.getCountryName();
+    }
+
+    public void loadCountryData(String filename) {
+        try {
+            FileInputStream inputFile = new FileInputStream(filename);
+            Reader in = new InputStreamReader(inputFile);
+            Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
+            for (CSVRecord record : records) {
+               countryRepository.save(CSVCountryDTO.countryFromCSV(record));
+            }
+        } catch (Exception e) {
+            LOGGER.error("Error loading Country data file in "+e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Return the hashcode of the prefix of the callsign
+     * @param prefix
+     * @return an intoeger representing the prefix.
+     */
+    private Integer getPrefixCode(String prefix) {
+        if (prefix.length() > 3 ) {
+            prefix = prefix.substring(0,3);
+        }
+
+        return prefix.hashCode();
+    }
+
+}
