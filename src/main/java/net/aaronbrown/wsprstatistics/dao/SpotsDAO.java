688769709220defb61ed7fc378d6be65c1762d9b
Aaron Brown <aaronbrown1988@gmail.com>
2019-02-23 11:25:09 +1100

    switch to std sql

--- src/main/java/net/aaronbrown/wsprstatistics/dao/SpotsDAO.java
+++ src/main/java/net/aaronbrown/wsprstatistics/dao/SpotsDAO.java
@@ -24,16 +24,16 @@ public class SpotsDAO {
 
     public Map<String, Statistics> statisticsByBand(String callSign) {
         String queryString = "SELECT band,avg(distance), max(distance),min(distance),count(spot_id),variance(distance) " +
-                "FROM [wsprstats-163301:wspr_data.spot_table] " +
-                "where Call_Sign='" + callSign + "' and band > 0 group by band";
+                "FROM `wsprstats-163301.wspr_data.spot_table` " +
+                "where callsign='" + callSign + "' and band > 0 group by band";
         TableResult result = bigQueryService.runQuery(queryString);
         return getStringStatisticsMap(result);
     }
 
     public Map<String, Statistics> statisticsByHour(String callSign, Integer band, Date start, Date end) {
-        String queryString = "SELECT hour(spot_time) as h,avg(distance), max(distance),min(distance),count(spot_id),variance(distance) " +
-                "FROM [wsprstats-163301:wspr_data.spot_table] " +
-                "where Call_Sign='" + callSign + "' and band=" + band + " and spot_time between " + start + " and " + end + " group by h";
+        String queryString = "SELECT hour(timestamp) as h,avg(distance), max(distance),min(distance),count(spot_id),variance(distance) " +
+                "FROM `wsprstats-163301.wspr_data.spot_table` " +
+                "where callsign='" + callSign + "' and band=" + band + " and timestamp between " + start + " and " + end + " group by h";
 
         TableResult result = bigQueryService.runQuery(queryString);
         return getStringStatisticsMap(result);
@@ -42,7 +42,7 @@ public class SpotsDAO {
     public Date lastUpdated() {
         Date updated = null;
         String queryString = "SELECT max(Timestamp)" +
-                "FROM [wsprstats-163301:wspr_data.spot_table] ";
+                "FROM `wsprstats-163301.wspr_data.spot_table` ";
         TableResult result = bigQueryService.runQuery(queryString);
         if (result.getTotalRows() != 1) {
             updated = null;
@@ -59,23 +59,24 @@ public class SpotsDAO {
     public Map<String, Double> getCountryByBand(String callsign, Integer band) {
         HashMap<String, Double> countries = new HashMap<>();
         String bandCriteria = (band != null) ? " and Band = " + band.toString() + " " : "";
-        String queryString = "select case when\n" +
-                "ref.country is not null then\n" +
-                "ref.country\n" +
-                "when ref1.country is not null then\n" +
-                "ref1.country\n" +
-                "when ref2.country is not null then\n" +
-                "ref2.country\n" +
-                "when ref3.country is not null then\n" +
-                "ref3.country\n" +
-                " else 'Unknown'" +
-                "end as country, count(data.spot_id)\n" +
-                "from (select * from (select spot_id, substr(reporter,0,1) as a,substr(reporter,0,2)as b,substr(reporter,0,3)as c,substr(reporter,0,4)as d \n" +
-                "from [wsprstats-163301:wspr_data.spot_table] where call_sign = '" + callsign + "'  " + bandCriteria + " ) as data\n" +
-                "left join [sprstats-163301:reference_data.big_cty] as ref on data.a = ref.prefix\n" +
-                "left join [sprstats-163301:reference_data.big_cty] as ref1 on data.b = ref1.prefix \n" +
-                "left join [sprstats-163301:reference_data.big_cty] as ref2 on data.c = ref2.prefix\n" +
-                "left join [sprstats-163301:reference_data.big_cty] as ref3 on data.d = ref3.prefix) group by country order by f0_ desc";
+        String queryString = "select case when\n"+
+                "ca is not null then\n"+
+                "ca\n"+
+                "when cb is not null then\n"+
+                "cb\n"+
+                "when cc is not null then\n"+
+                "cc\n"+
+                "when cd is not null then\n"+
+                "cd\n"+
+                "else 'Unknown'end as country, count(spot_id) as total\n"+
+                "from\n"+
+                "(select * from (select spot_id, substr(reporter,0,1) as a,substr(reporter,0,2)as b,substr(reporter,0,3)as c,substr(reporter,0,4)as d \n"+
+                "from `wsprstats-163301.wspr_data.spot_table` where callsign ='" + callsign + "'  " + bandCriteria + " ) as data\n"+
+                "left join (select country as ca, prefix as pa from `wsprstats-163301.reference_data.big_cty`) as ref on data.a = ref.pa\n"+
+                "left join (select country as cb, prefix as pb from `wsprstats-163301.reference_data.big_cty`) as ref1 on data.b = ref1.pb \n"+
+                "left join (select country as cc, prefix as pc from `wsprstats-163301.reference_data.big_cty`) as ref2 on data.c = ref2.pc\n"+
+                "left join (select country as cd, prefix as pd from `wsprstats-163301.reference_data.big_cty`) as ref3 on data.d = ref3.pd)\n"+
+                "group by country order by total desc";
 
         TableResult result = bigQueryService.runQuery(queryString);
 
@@ -88,9 +89,14 @@ public class SpotsDAO {
     }
 
     private Map<String, Statistics> getStringStatisticsMap(TableResult result) {
+        HashMap<String, Statistics> stats = new HashMap<>();
+
+        if (result == null ) {
+            return stats;
+        }
         Iterable<FieldValueList> iter = result.iterateAll();
 
-        HashMap<String, Statistics> stats = new HashMap<>();
+
 
         for (FieldValueList row : iter) {
             Statistics current = new Statistics();
