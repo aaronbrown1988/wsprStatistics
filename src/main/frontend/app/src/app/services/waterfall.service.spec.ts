5597ef88c3f056308559f6ed9280b8d1ac117c2f
Aaron Brown <aaronbrown1988@gmail.com>
2019-09-01 10:32:31 +1000

    collect audio and pass it through and fft.

--- src/main/frontend/app/src/app/services/waterfall.service.spec.ts
+++ src/main/frontend/app/src/app/services/waterfall.service.spec.ts
@@ -9,4 +9,26 @@ describe('WaterfallService', () => {
     const service: WaterfallService = TestBed.get(WaterfallService);
     expect(service).toBeTruthy();
   });
+
+  it('should be initialisable', () => {
+    const service: WaterfallService = TestBed.get(WaterfallService)
+    expect(service.initAudioRecorder()).not.toBeNull();
+    expect(service['stream']).not.toBeNull()
+  });
+
+  it('should stop listening when its done', () => {
+    const service: WaterfallService = TestBed.get(WaterfallService)
+    service.initAudioRecorder();
+    expect(service.closeAudio()).toBeTruthy()
+    
+  }
+  )
+
+  it('should capture raw audio', () => {
+    const service: WaterfallService = TestBed.get(WaterfallService)
+    service.initAudioRecorder();
+    let samples = service.getSamples()
+    expect(samples).toBeTruthy();
+    console.dir(samples)
+  })
 });
--- src/main/frontend/app/src/app/services/waterfall.service.ts
+++ src/main/frontend/app/src/app/services/waterfall.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { RecordRTCPromisesHandler} from 'recordrtc'
+import { RecordRTCPromisesHandler } from 'recordrtc'
+import {fft} from 'fft-js'
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,36 @@ export class WaterfallService {
 
 
 
-  initAudioRecorder = async function() {
-    let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-    let recorder = new RecordRTCPromisesHandler(stream, {
+  initAudioRecorder = async function () {
+    this.stream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true });
+    this.recorder = new RecordRTCPromisesHandler(this.stream, {
       type: 'audio'
     });
+    return this.stream
   }
+
+  closeAudio = async function() {
+    this.recorder.destory();
+    this.stream.stop();
+  }
+
+  getSamples = async function () {
+    this.recorder.startRecording();
+    const sleep = m => new Promise(r => setTimeout(r, m));
+    await sleep(3000);
+
+    await this.recorder.stopRecording();
+    let blob = await this.recorder.getBlob();
+    console.log(`Done collecting audio samples`)
+    return blob
+  }
+
+  decode = async function() {
+    let samples = this.getSamples();
+    let phasors = fft(samples)
+    console.log(`Calculated the following FFT ${phasors}`)
+    
+  }
+
+
 }
